55e83308bf6632f0d25a43972d749837a67bf557
MaximusXYZ <61899298+MaximusXYZ@users.noreply.github.com>
2021-01-22 08:59:59 +0800

    Create PipeInterface.cs

---
+++ PipeInterface.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.IO.Pipes;
+
+namespace OmegaX.Interfaces
+{
+    public partial class PipeInterface
+    {
+        public string PipeName;
+        public int Timeout;
+        
+        private WebClient Web { get; } = new();
+
+        public void SendData(string Data, Uri Source = null, bool IsUri = false)
+        {
+            if (PipeName is null || PipeName == "") throw new Exception("A pipename cannot be empty.");
+            if (Data is null) return;
+            if (Source is null) return;
+
+            //check pipefirst
+
+            try
+            {
+                using (var ClientStream = new NamedPipeClientStream(".", PipeName, PipeDirection.Out))
+                {
+                    ClientStream.Connect(Timeout);
+
+                    using (var Stream = new StreamWriter(ClientStream))
+                    {
+                        if (Data is not null && IsUri != true) Stream.Write(Data);
+                        else if (Source is not null && IsUri != false) Stream.Write(Web.DownloadString(Source));
+
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
